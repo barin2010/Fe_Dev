fbf9d25bd5ee8253d1d4e9c9b995edd19d2df728
barin2010 <uyrirudenko@gmail.com>
2024-07-30 02:52:57 +0300

    connected video

--- src/components/VideoPlayer/VideoPlayer.jsx
+++ src/components/VideoPlayer/VideoPlayer.jsx
@@ -1,42 +1,37 @@
-// import React, { useRef } from 'react';
 import React, { useRef, useState } from 'react';
+import ReactPlayer from 'react-player';
 import css from './VideoPlayer.module.css';
 import mute from '../../images/icons/mute.svg';
 import muteX from '../../images/icons/muteX.svg';
 
 const VideoPlayer = () => {
-  const videoRef = useRef(null);
-
-  const [isMuted, setIsMuted] = useState(true);
+  const playerRef = useRef(null);
+  const [isMuted, setIsMuted] = useState(false);
 
   const handleMuteToggle = () => {
-    if (videoRef.current) {
-      videoRef.current.muted = !videoRef.current.muted;
-      setIsMuted(videoRef.current.muted);
-    }
+    setIsMuted(prevState => !prevState);
   };
 
   return (
     <div className={css.videoPlayer}>
-      <div className={css.playeerWrapper}>
-        <video
-          className={css.playeer}
-          ref={videoRef}
-          src="../../downloads/42318-431511222_tiny.mp4"
-          type="video/mp4"
-          autoPlay
+      <div className={css.playerWrapper}>
+        <ReactPlayer
+          className={css.player}
+          ref={playerRef}
+          url="https://www.youtube.com/watch?v=wnhvanMdx4s"
+          playing
           loop
-          muted
+          muted={isMuted}
           controls
-          style={{ width: '100%', height: 'auto' }}
-        >
-          Your browser does not support the video tag.
-        </video>
+          width="100%"
+          height="100%"
+          style={{ aspectRatio: '16/9' }}
+        />
         <button className={css.buttonMute} onClick={handleMuteToggle}>
           {isMuted ? (
             <img src={mute} alt="mute" />
           ) : (
-            <img src={muteX} alt="mute" />
+            <img src={muteX} alt="unmute" />
           )}
         </button>
       </div>
